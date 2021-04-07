963bbae70ba2202896de51993ccbabfc55e5db18
Nenad Damnjanović <nenad.default@gmail.com>
2021-04-07 22:43:59 +0200

    Optimize caching -- use MSET and do it in chunks

--- src/cache-words.py
+++ src/cache-words.py
@@ -1,11 +1,13 @@
 import redis
+import math
+from pprint import pprint
 
 def get_words_from_dict():
 
-  with open('dictionaries/words.txt') as word_file:
-    valid_words = set(word_file.read().split())
+  with open('../dictionaries/words.txt') as word_file:
+    valid_words = list(word_file.read().split())
 
-  return valid_words
+  return valid_words # 466550 words
 
 def cache_words(words):
 
@@ -13,8 +15,14 @@ def cache_words(words):
 
   r.flushdb()
 
-  for key in words:
-    r.set(key, 0)
+  word_count = len(words)
+  chunk_size = 1000
+  chunks = math.ceil(word_count / chunk_size)
+
+  for i in range(chunks):
+    chunk_start = i * chunk_size
+    chunk = words[chunk_start:chunk_start + chunk_size]
+    r.mset({word: 0 for word in chunk})
 
 if __name__ == '__main__':
   english_words = get_words_from_dict()
